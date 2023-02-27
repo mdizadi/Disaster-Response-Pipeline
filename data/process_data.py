8fe430ef43ea8bc2cda6f2ecc2ae30aabe91c863
mdizadi <mdizadi@gmail.com>
2023-02-27 08:59:04 +0100

    reset commit and added .gitignore.

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,77 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    # load messages dataset
+    #messages = pd.read_csv(messages_filepath + '/disaster_messages.csv')
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    #categories = pd.read_csv(categories_filepath + '/disaster_categories.csv')
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = messages.merge(categories, how = 'inner', on = 'id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand = True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x : x[:-2])
+
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str.replace(column + '-', '')
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    for column in categories.columns:
+        b = categories[column].value_counts().index
+        if ((1 & 0) in b) & (len(b) == 2):
+            pass
+        else:
+            print('There is a problem with values of the column "{}", and it has {} distinct value(s)!'.format(column, len(b)))
+
+    categories['related'] = categories['related'].apply(lambda x : 1 if x == 2 else x)
+
+    b = categories['child_alone'].value_counts().index
+    if not(((1 & 0) in b) & (len(b) == 2)):
+        categories = categories.drop(['child_alone'], axis = 1)
+
+    # drop the original categories column from `df    
+    df = df.drop('categories', axis = 1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis = 1)
+
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    # duplicate messages
+    df = df.drop_duplicates(subset='message')
+    
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filepath):
+    #engine = create_engine(f'sqlite:///{database_filepath}' + '/DisasterResponse.db')
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df.to_sql('DisasterResponse', con=engine, if_exists='replace', index=False)  
 
 
 def main():
@@ -25,7 +86,7 @@ def main():
         print('Cleaning data...')
         df = clean_data(df)
         
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        print('Saving data...\n    DATABASE: {}/DisasterResponse.db'.format(database_filepath))
         save_data(df, database_filepath)
         
         print('Cleaned data saved to database!')
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,89 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'vader_lexicon'])
+import sqlite3
+import re
+import numpy as np
+import pandas as pd
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+
+import pickle
+
 
 
 def load_data(database_filepath):
-    pass
+    conn = sqlite3.connect(database_filepath)
+    df = pd.read_sql('SELECT * FROM DisasterResponse', con=conn)
 
+    X = df['message']
+    Y = df.loc[:, 'related':'direct_report']
+
+    category_names = Y.columns
+
+    conn.close()
+
+    return X, Y, category_names
+
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 def tokenize(text):
-    pass
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('multi_clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    #X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+
+    #pipeline.fit(X_train, Y_train)
+
+    return pipeline
+
+
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    i = 0
+    report = []
+    for column in category_names:
+        colreport = classification_report(np.array(Y_test[column]),Y_pred[:,i])
+        print(column + '\n' , colreport)
+        report.append(colreport)
+        i += 1
+
+    return report
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
